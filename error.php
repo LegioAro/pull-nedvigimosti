83b624d5d582edaaa29aa76b002dc8b80fc0581d
LegionAro <u@example.com>
2023-02-27 19:25:51 +0200

    add page error, success

---
+++ error.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/styles.css" />
+    <title>Не успешная регистрация</title>
+  </head>
+  <body>
+    <header class="header header__reg">
+      <div class="container">
+        <div class="header__wrapper">
+          <div class="header__content">
+            <div class="header__logo"><img src="img/logo.svg" alt="" /></div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <section class="registration">
+      <div class="container">
+        <div class="registration__wrapper">
+          <div class="registration__img false"><span class="icon-close-outline"></span></div>
+          <div class="registration__title">
+            <div class="title">
+              <span class="--select"> ВНИМАНИЕ!</span> <br />Мы не можем запросить для вас
+              консультацию.
+            </div>
+          </div>
+          <div class="registration__text">
+            Пожалуйста, перейдите по первоначальной ссылке, которую вам дал пригласивший вас человек, и затем повторите путь снова:
+          </div>
+          <div class="registration__list">
+            <div class="list list__registration">
+              <div class="list__item">
+                <div class="list__item-dec"></div>
+                <div class="list__item-text">
+                  заполните корректно все поля в форме, нажав на кнопку "Получить консультацию"
+                </div>
+              </div>
+              <div class="list__item">
+                <div class="list__item-dec"></div>
+                <div class="list__item-text">
+                  получите подтверждение, что заявка на консультацию принята ✅
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="registration__text">
+            Ваш Наставник свяжется с вами в ближайшее время и ответит на все ваши вопросы.
+          </div>
+          <div class="registration__btn">
+            <a href="index.php" class="btn">на главную <span class="icon-arrow-btn"></span></a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
---
+++ success.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/styles.css" />
+    <title>Успешная регистрация</title>
+  </head>
+  <body>
+    <header class="header header__reg">
+      <div class="container">
+        <div class="header__wrapper">
+          <div class="header__content">
+            <div class="header__logo"><img src="img/logo.svg" alt="" /></div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <section class="registration">
+      <div class="container">
+        <div class="registration__wrapper">
+          <div class="registration__img"><span class="icon-check-circle"></span></div>
+          <div class="registration__title">
+            <div class="title">
+              ВАША ЗАЯВКА НА КОНСУЛЬТАЦИЮ <span class="--select"> УСПЕШНО ПРИНЯТА</span>
+            </div>
+          </div>
+          <div class="registration__text">
+            Ваши данные переданы 
+            Наставнику. Он свяжется с вами в ближайшее время!
+          </div>
+          <div class="registration__btn">
+            <a href="index.php" class="btn">на главную <span class="icon-arrow-btn"></span></a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
